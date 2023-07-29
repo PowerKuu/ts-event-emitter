2b5f09bd4ea04e61371fa7d592f9b13f027aafdf
PowerKuu <hkleven6@outlook.com>
2023-07-29 22:13:35 +0200

    upadate

--- dist/main.d.ts
+++ dist/main.d.ts
@@ -1,8 +1,8 @@
-export declare type EmittedEvents = Record<string | symbol, (...args: any) => any>;
+export declare type EmittedEvents = Record<string | symbol, (...args: any[]) => any>;
 export default class TsEventEmitter<Events extends EmittedEvents> {
     events: {
         [K in keyof Events]?: Events[K][];
     };
-    emit<E extends keyof Events>(event: E, ...args: Parameters<Events[E]>[]): void;
+    emit<E extends keyof Events>(event: E, ...args: Parameters<Events[E]>): void;
     on<E extends keyof Events>(event: E, cb: Events[E]): void;
 }
--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
-export type EmittedEvents = Record<string | symbol, (...args: any) => any>
+export type EmittedEvents = Record<string | symbol, (...args: any[]) => any>
 
 export default class TsEventEmitter<Events extends EmittedEvents> {
     public events: {
         [K in keyof Events]?: Events[K][]
     } = {}
 
-    public emit<E extends keyof Events>(event: E, ...args: Parameters<Events[E]>[]) {
+    public emit<E extends keyof Events>(event: E, ...args: Parameters<Events[E]>) {
         const callbacks = this.events[event] || []
 
         callbacks.forEach(cb => cb(...args))
